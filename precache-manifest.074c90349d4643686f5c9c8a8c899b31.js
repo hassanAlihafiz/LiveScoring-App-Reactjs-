9190605b92b515e8144e01fd41d96f491077ea7c
hassanAlihafiz <bali37088@gmail.com>
2020-10-10 10:15:20 -0700

    Updates

---
+++ precache-manifest.074c90349d4643686f5c9c8a8c899b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1e2788e8be3806152b8ffa1d4fc6fd",
+    "url": "/hassanAlihafiz.github.io/reactproject2/index.html"
+  },
+  {
+    "revision": "e5829e2b94bae3bf6c40",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/css/main.94320a43.chunk.css"
+  },
+  {
+    "revision": "962cac49f6c167b2f897",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/js/2.89db4e8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/js/2.89db4e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5829e2b94bae3bf6c40",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/js/main.baae3aa7.chunk.js"
+  },
+  {
+    "revision": "f4b92ced5e9571ae4a3e",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/js/runtime-main.858018c5.js"
+  },
+  {
+    "revision": "27b23a52df0c85503b9fe1b36595cb5a",
+    "url": "/hassanAlihafiz.github.io/reactproject2/static/media/vs.27b23a52.png"
+  }
+]);
